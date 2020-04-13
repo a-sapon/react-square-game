583be7d5b85ac388256557fdc334998f2e7a1be4
a-sapon <sapon.anya@gmail.com>
2020-04-13 19:06:20 +0300

    Updates

---
+++ precache-manifest.41c7f7cdc02cca7025fd108ed9a03121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e14beae8d2f7a7dba935a3745ecbc3",
+    "url": "/square-game/index.html"
+  },
+  {
+    "revision": "32affea0d0389c3ae4da",
+    "url": "/square-game/static/css/main.d31ec33e.chunk.css"
+  },
+  {
+    "revision": "cacb830cc4d620ab6bad",
+    "url": "/square-game/static/js/2.87d48963.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/square-game/static/js/2.87d48963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32affea0d0389c3ae4da",
+    "url": "/square-game/static/js/main.6a45d124.chunk.js"
+  },
+  {
+    "revision": "25476ca2ac042a54a05a",
+    "url": "/square-game/static/js/runtime-main.1de97a5f.js"
+  }
+]);
